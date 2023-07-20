796d8e27d26ca21051f8d9f7233b34fd7fa8cceb
Dan Harrin <git@danharrin.com>
2023-07-20 12:37:47 -0500

    Update v3when.js

--- commands/v3when.js
+++ commands/v3when.js
@@ -5,42 +5,12 @@ module.exports = {
         .setName("v3when")
         .setDescription("When is v3 coming out?"),
     async execute(interaction) {
-        await interaction.reply(`**Beta**: ${beta()}\n**Release**: ${release()}`)
+        await interaction.reply(`**Beta**: https://github.com/filamentphp/filament/releases/tag/v3.0.0-beta1\n**Release**: ${release()}`)
     }
 }
 
 function formatCounter(number) {
     return number.toString().padStart(2, '0')
-}
-
-function beta() {
-    let countDownDate = new Date(2023, 6, 20, 19).getTime()
-    let timeDistance = countDownDate - new Date().getTime()
-
-    if (timeDistance < 0) {
-        clearInterval(this.runningCounter)
-
-        return
-    }
-
-    const values = {
-        days: formatCounter(
-            Math.floor(timeDistance / (1000 * 60 * 60 * 24)),
-        ),
-        hours: formatCounter(
-            Math.floor(
-                (timeDistance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-            ),
-        ),
-        minutes: formatCounter(
-            Math.floor((timeDistance % (1000 * 60 * 60)) / (1000 * 60)),
-        ),
-        seconds: formatCounter(
-            Math.floor((timeDistance % (1000 * 60)) / 1000),
-        )
-    }
-
-    return `${values.days}d ${values.hours}h ${values.minutes}m ${values.seconds}s`
 }
 
 function release() {
